386c43d03ae5459f9bd335d6ec69e64431a5a235
kyleaune <aune.kyle@gmail.com>
2025-05-30 11:42:13 -0400

    including U codes for cause of death, plotting covid separately

--- subgroup_analyses.R
+++ subgroup_analyses.R
@@ -1,16 +1,22 @@
+#----------------------------------------------------------------#
+#               2025 CSTE Applied Trend Analysis in R            #
+# Subgroup Analysis Strategies - Demonstration & Guided Practice #
+# Authors: Johns Hopkins Surveillance and Outbreak Response Team #
+#----------------------------------------------------------------#
+
 #---- Setup #----
 
 # List of package names as characters
 pkgs <- c("tidyverse", "ISOweek", "excessmort", "ggpubr", "openxlsx")
 
-# Download and install any pacakges that are missing
+# Download and install any packages that are missing
 install.packages(setdiff(pkgs, installed.packages()), install.packages())
 
 # Load packages
 lapply(pkgs, library, character.only = TRUE)
 
 # Read in mortality data set
-mort.dat <- readRDS("linelist_2010-2020_mi.rda") %>%
+mort.dat <- read_csv("linelist_2010-2020_mi.csv") %>%
   # Since we want to run the analysis over weekly counts of observed vs. expected
   # deaths, we need to figure out which week each death occurred in. We'll assign
   # each date of death to a week of death
@@ -71,7 +77,7 @@ demo.total <- approx_demographics(
 pandemic.dates <- seq.Date(as.Date("2020-03-10"), as.Date("2020-12-31"), by = "day")
 
 
-#---- Subanalysis by Cause of Death #----
+#---- Sub-analysis by Cause of Death #----
 
 # The data provided by CDC Wonder lists 19 causes of death, but the frequency of
 # each cause varies pretty substantially - there are lots of cardiovascular and
@@ -81,7 +87,7 @@ pandemic.dates <- seq.Date(as.Date("2020-03-10"), as.Date("2020-12-31"), by = "d
 
 # Let's take a look at the causes that are experienced to see how we want to group.
 # things together
-table(mort.dat$cause) / nrow(mort.dat) * 100
+sort(table(mort.dat$cause) / nrow(mort.dat) * 100, decreasing = TRUE)
   # Let's move pregnancy and perinatal together, then group anything under 1% as 'other'
 
 # Further categorizing cause of death
@@ -89,10 +95,13 @@ mort.dat <- mort.dat %>%
   # We're going to use the 'case_when' function which is basically like a lot of
   # nested 'if, else' statements
   mutate(
-    cause.cat = case_when(cause %in% c("Pregnancy", "Perinatal") ~ "Pregnancy & Perinatal",
-                          cause %in% c("Blood & Immune Disorders", "Congenital Malformation",
-                                       "Ear Disease", "Eye Disease", "Musculoskeletal",
-                                       "Skin Disease") ~ "Other",
+    cause.cat = case_when(cause %in% c("Pregnancy (O00-O99)", "Perinatal (P00-P96)") ~
+                            "Pregnancy & Perinatal (O00-P96)",
+                          cause %in% c("Blood & Immune Disorders (D50-D89)",
+                                       "Congenital Malformation (Q00-Q99)",
+                                       "Ear Disease (H60-H93)", "Eye Disease (H00-H57)",
+                                       "Musculoskeletal (M00-M99)", "Skin Disease (L00-L98)",
+                                       "Abnormal (R00-R99)") ~ "Other",
                           # We can now set everything else to stay the same
                           .default = cause))
 
@@ -137,7 +146,7 @@ weekly.xs.cause <- lapply(
                      start = min(.$date),
                      end = max(.$date),
                      knots.per.year = 12,
-                     include.trend = FALSE,
+                     include.trend = TRUE,
                      frequency = 52,
                      model = "quasipoisson",
                      alpha = 0.05,
@@ -175,7 +184,12 @@ weekly.cause.ts <- lapply(
 # graph. Because we're using more than one list, we have to adapt our code to use
 # 'mapply' (for multivariate apply) instead of 'lapply' like above. It's structured
 # a little different, but the idea is generally the same. This time though, we can
-# define more than one variable like you'll see below.
+# define more than one variable like you'll see below. Also, since we're plotting
+# observed vs. expected deaths, this setup doesn't make much sense for visualizing
+# COVID-19 caused deaths (ICD-10 chapter U codes), since there's no historical data
+# to determine what would be "expected" (in reality, we would expect zero deaths).
+# So we'll plot all the other, non-COVID-19 causes here, and plot observed COVID-19
+# deaths by themselves.
 
 # Plotting observed vs expected deaths from 1/1/2020 - 12/31/2020
 xs.cause.plots <- mapply(
@@ -220,11 +234,45 @@ xs.cause.plots <- mapply(
     )
   },
   # Now we'll specify what 'x' and 'y' represent in the function above
-  x = weekly.cause.ts,
-  y = names(weekly.cause.ts),
+  # 'x' will be the cause time series that we cleaned up, but we'll skip COVID-19
+  # deaths. Since those are stored as the 14th element in the 'weekly.cause.ts'
+  # list, we can drop it
+  x = weekly.cause.ts[-14],
+  # 'y' is used to generate plot titles and is the names of the elements of the
+  # 'weekly.cause.ts' list (which are the names of the cause of death categories).
+  # We still don't want to include COVID-19 deaths here, so we'll drop them.
+  y = names(weekly.cause.ts[-14]),
   # Finally, we have to ask R to not wrap the results up in a list for us (the default)
   SIMPLIFY = FALSE
 )
+
+# Now let's plot just the observed COVID-19 deaths (without including an expected
+# curve).
+# We need to reshape the dataframe before we can plot it
+covid.plotdat <- weekly.cause.ts[[14]] %>%
+  filter(date > make_date(2020, 01, 01))
+
+covid.plot <- ggplot(covid.plotdat, aes(x = date, y = observed, lty = "Observed")) +
+  geom_line(col = "#F8766D") +
+  scale_x_date(
+    date_breaks = "1 month",
+    date_minor_breaks = "1 week",
+    date_labels = "%b %Y") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        panel.grid.minor = element_blank(),
+        legend.title = element_blank()) +
+  xlab("Date") +
+  ylab("Weekly Death Count") +
+  labs(col = NULL,
+       title = "Observed Mortality 2020",
+       subtitle = paste(names(weekly.cause.ts[14]), "Cause of Death", sep = " "))
+
+# Let's add the plot of COVID-19 deaths back to our full list of plots by cause
+# of death.
+xs.cause.plots <- c(xs.cause.plots, list(covid.plot))
+# Now let's give each plot a name (that we can use to easily refer to each plot
+# or use in filenames when saving).
+names(xs.cause.plots) <- names(weekly.cause.ts)
 
 # Now that we have nice figures, let's save high-quality versions so we can share
 # them in reports and presentations. With a single figure, we could write one
@@ -273,6 +321,10 @@ cause.table <- mapply(function(x, y) {
            # Calculating excess confidence interval
            mutate(excess.l95 = excess - qnorm(1-(.05/2)) * se,
                   excess.u95 = excess + qnorm(1-(.05/2)) * se) %>%
+           # Calculating mortality ratio (observed / expected) & 95% CI
+           mutate(mr = observed / expected,
+                  mr.l95 = observed / (expected + qnorm(1-(.05/2)) * se),
+                  mr.u95 = observed / (expected - qnorm(1-(.05/2)) * se)) %>%
            # Giving each row the name of the cause of death it represents
            mutate(cause = y) %>%
            # Moving the cause column all the way to the left
@@ -292,11 +344,149 @@ cause.table <- mapply(function(x, y) {
 write.xlsx(cause.table, "excess_deaths_by_cause.xlsx", asTable = TRUE)
 
 
+#---- Sub-analysis by County #----
 
-#---- Subanalysis by County #----
+# The full dataset 'mort.dat' is also recorded at the county level, so let's see
+# if there were any big differences in 2020 excess deaths by county
 
-# For county-specific rates of excess mortality, we'll use the same type of setup,
-# but this time we have a row for every county's population in every year.
+# For county-specific rates of excess mortality, we'll use the same type of setup
+# for demographic data, but this time we have a row for every county's population
+# in every year.
 
 # Read in county-level demographic data
 demo.county.in <- read_csv("demo_by_year_by_county_2010-2020_MI.csv")
+
+# But since there are 83 counties in Michigan, let's make things a little easier
+# on ourselves and select four counties to look at. How about two urban and two
+# rural. Let's look at Wayne County (Detroit), Kent County (Grand Rapids), 
+# Marquette (in the Upper Pensinsula), and Lapeer (outside Detroit)
+
+# Filter to these counties
+mort.dat.co <- mort.dat %>%
+  filter(county %in% c("Wayne County, MI", "Kent County, MI",
+                       "Marquette County, MI", "Lapeer County, MI"))
+
+# Let's filter the demographics too
+demo.county.in.sub <- demo.county.in %>%
+  filter(county %in% c("Wayne County, MI", "Kent County, MI",
+                       "Marquette County, MI", "Lapeer County, MI"))
+
+# We need to take that table of annual population by county and create a long
+# dataframe with daily population.
+demo.county <- approx_demographics(demo.county.in.sub,
+                                   first_day = min(mort.dat$isodate),
+                                   last_day = max(mort.dat$isodate),
+                                   by = "county",
+                                   extrapolation.type = "linear")
+
+# Count how many deaths occurred in each county, each week
+county.counts <- compute_counts(
+  dat = mort.dat.co,
+  demo = demo.county,
+  date = "isodate",
+  by = "county")
+
+# Split the mortality data by county into a list of dataframes
+county.counts.ls <- split(county.counts, f = county.counts$county)
+
+# Run the excess model function on each item in the list
+weekly.xs.county <- lapply(
+  county.counts.ls, 
+  function(x) {
+    return(
+      x %>%
+        excess_model(exclude = pandemic.dates,
+                     start = min(.$date),
+                     end = max(.$date),
+                     knots.per.year = 12,
+                     include.trend = FALSE,
+                     frequency = 52,
+                     model = "quasipoisson",
+                     alpha = 0.05,
+                     keep.counts = TRUE)
+    )
+  })
+
+# Restructure the results for plotting
+weekly.county.ts <- lapply(
+  weekly.xs.county,
+  function(x) {
+    x <- bind_cols(x[c("date", "observed", "expected", "fitted", "log_expected_se")]) %>%
+      mutate(exp.l95 = exp(log(expected) - qnorm(1-(.05/2)) * log_expected_se),
+             exp.u95 = exp(log(expected) + qnorm(1-(.05/2)) * log_expected_se))
+  }
+)
+
+# Plot excess death by county
+xs.county.plots <- mapply(
+  function(x, y) {
+    x <- x %>%
+      filter(date > make_date(2020, 01, 01)) %>%
+      pivot_longer(cols = c(observed:expected),
+                   names_to = "class",
+                   values_to = "value") %>%
+      mutate(class = factor(class, levels = c("observed", "expected"),
+                            labels = c("Observed", "Expected")))
+    
+    return(
+      ggplot(x, aes(x = date, y = value, col = class)) +
+        geom_line() +
+        geom_ribbon(data = x %>% filter(class == "Expected"),
+                    aes(ymin = exp.l95, ymax = exp.u95),
+                    fill = NA, lty = 2) +
+        scale_x_date(
+          date_breaks = "1 month",
+          date_minor_breaks = "1 week",
+          date_labels = "%b %Y") +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1),
+              panel.grid.minor = element_blank()) +
+        xlab("Date") +
+        ylab("Weekly Death Count") +
+        labs(col = NULL,
+             title = "Excess Mortality 2020",
+             subtitle = y)
+    )
+  },
+  x = weekly.county.ts,
+  y = names(weekly.county.ts),
+  SIMPLIFY = FALSE
+)
+
+# Save the figures for each county
+mapply(
+  function(x, y) {
+    ggsave(y, plot = x,
+           width = 6.5, height = 3, units = "in", dpi = 300)
+  },
+  x = xs.cause.plots, 
+  y = paste0("excess_",
+             gsub(" ", "_",
+                  gsub(" & ", "-", names(xs.cause.plots))),
+             "_deaths.png"))
+
+# Create a table of statistics for each county
+county.table <- mapply(function(x, y) {
+  return(x %>%
+           excess_model(exclude = pandemic.dates,
+                        intervals = list(pandemic.dates),
+                        knots.per.year = 12,
+                        include.trend = FALSE,
+                        model = "quasipoisson",
+                        alpha = 0.05) %>%
+           mutate(se = sd / sqrt(observed)) %>%
+           mutate(excess.l95 = excess - qnorm(1-(.05/2)) * se,
+                  excess.u95 = excess + qnorm(1-(.05/2)) * se) %>%
+           mutate(mr = observed / expected,
+                  mr.l95 = observed / (expected + qnorm(1-(.05/2)) * se),
+                  mr.u95 = observed / (expected - qnorm(1-(.05/2)) * se)) %>%
+           mutate(cause = y) %>%
+           relocate(cause) %>%
+           mutate(xs_rate = obs_death_rate - exp_death_rate))
+  }, 
+  x = county.counts.ls,
+  y = names(county.counts.ls),
+  SIMPLIFY = FALSE) %>%
+  bind_rows()
+
+# Save the table as an Excel file
+write.xlsx(county.table, "excess_deaths_by_county.xlsx", asTable = TRUE)
